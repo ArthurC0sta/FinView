4ec5352f10c7b80657d8394fadb82f453537e907
Arthur Costa <arthur.costa@treo.com.br>
2026-04-29 12:13:41 -0300

    ajuste repositorio

--- gastos/urls.py
+++ gastos/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('monthly/', views.monthly, name='monthly'),
-    path('monthly/delete-income/', views.delete_monthly_income, name='delete_monthly_income'),
     path('new-expense/', views.new_expense, name='new_expense'),
-    path('expenses/<int:expense_id>/delete/', views.delete_expense, name='delete_expense'),
     path('profile/', views.profile, name='profile'),
 ]
